10322fe39e88f2456b7000bfaeab0960e7d664e9
saresend <saresend@usc.edu>
2019-09-17 15:31:40 -0700

    beginning to add database integration

--- main.js
+++ main.js
@@ -1,52 +1,54 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const Sequelize = require('sequelize');
+
 
 const user_data = [
-    {
-      "picture": "http://placehold.it/32x32",
-      "age": 30,
-      "name": {
-        "first": "Berger",
-        "last": "Ferguson"
-      }
-    },
-    {
-      "picture": "http://placehold.it/32x32",
-      "age": 23,
-      "name": {
-        "first": "Sparks",
-        "last": "Leach"
-      }
-    },
-    {
-      "picture": "http://placehold.it/32x32",
-      "age": 30,
-      "name": {
-        "first": "Andrews",
-        "last": "Berger"
-      }
-    }
-]; 
+	{
+		"picture": "http://placehold.it/32x32",
+		"age": 30,
+		"name": {
+			"first": "Berger",
+			"last": "Ferguson"
+		}
+	},
+	{
+		"picture": "http://placehold.it/32x32",
+		"age": 23,
+		"name": {
+			"first": "Sparks",
+			"last": "Leach"
+		}
+	},
+	{
+		"picture": "http://placehold.it/32x32",
+		"age": 30,
+		"name": {
+			"first": "Andrews",
+			"last": "Berger"
+		}
+	}
+];
 
 const event_data = [
 	{
-		name: 'Welcome to my Ted Talk', 
-		room: 342, 
-		presenter: user_data[0], 
-		start: 12, 
+		name: 'Welcome to my Ted Talk',
+		room: 342,
+		presenter: user_data[0],
+		start: 12,
 		end: 13,
 	},
 	{
-		name: 'Underwater basketweaving for dummies', 
-		room: 222, 
+		name: 'Underwater basketweaving for dummies',
+		room: 222,
 		presenter: user_data[1],
 		start: 14,
 		end: 16,
-	}, 
+	},
 	{
-		name: 'The history and origins of Squid', 
-		room: 124, 
-		presenter: user_data[2], 
+		name: 'The history and origins of Squid',
+		room: 124,
+		presenter: user_data[2],
 		start: 8,
 		end: 9,
 	},
@@ -74,24 +76,39 @@ const typeDefs = gql`
 		users: [User],
 		events: [Event],
 		serverSource: String,
-  }
+	}
+	type Mutation {
+		changeHello(hello: String!): String
+	}
 `;
+var hello_str = 'Hello world!';
 
 // Provide resolver functions for your schema fields
 const resolvers = {
-  Query: {
+	Query: {
 		events: () => event_data,
-		users: () => user_data,		
-    hello: () => 'Hello world!',
+		users: () => user_data,
+		hello: () => hello_str,
 		serverSource: () => 'https://github.com/saresend/apollo-server-PoC',
-  },
+	},
+	Mutation: {
+		changeHello: (root, args) => {
+			hello_str = args.hello;
+			return hello_str;
+		}
+	}
 };
-
+const sequelize = new Sequelize('postgres://localhost:5432/template1');
 const server = new ApolloServer({ typeDefs, resolvers });
 
+sequelize.authenticate().then(() => {
+	console.log('Successfully Authenticated to database')
+}).catch(err => {
+	console.log('failed because' + err);
+})
 const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ host: '0.0.0.0', port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
